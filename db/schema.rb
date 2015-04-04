ebd4f6a90c5dfb062e0bb20061bf3d1872bf6282
Mike Agrapides <mike.agrapides@gmail.com>
2015-04-04 17:28:20 +0100

    added name field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402221419) do
+ActiveRecord::Schema.define(version: 20150404161305) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150402221419) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
