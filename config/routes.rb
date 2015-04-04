69518d831f749aba103fafe72dd5f55f78013bfc
Mike Agrapides <mike.agrapides@gmail.com>
2015-04-04 10:20:12 +0100

    Update show view, add glyphicons, and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pins
 
   devise_for :users
-  root "pages#home"
+  root "pins#index"
   get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
